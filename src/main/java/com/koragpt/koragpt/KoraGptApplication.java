d5733767cea8705af5da01d99fbf488f5e42dc7a
kosta <kostasmpous@gmail.com>
2025-07-27 18:55:29 +0300

    initial commit

---
+++ src/main/java/com/koragpt/koragpt/KoraGptApplication.java
@@ -0,0 +1,13 @@
+package com.koragpt.koragpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoraGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoraGptApplication.class, args);
+    }
+
+}
