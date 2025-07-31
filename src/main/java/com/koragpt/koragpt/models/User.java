53613f704e30a837e813e1175271afae38b6d2b4
kosta <kostasmpous@gmail.com>
2025-08-01 01:09:24 +0300

    initial commit

---
+++ src/main/java/com/koragpt/koragpt/models/User.java
@@ -0,0 +1,51 @@
+package com.koragpt.koragpt.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.security.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "active")
+    private boolean active;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = this.updatedAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
+
+
---
+++ src/main/java/com/koragpt/koragpt/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.koragpt.koragpt.repositories;
+
+import com.koragpt.koragpt.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
